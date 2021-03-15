a22388220d76f4e456bcd90a2790da3cb7076928
Artemis21 <hollyshort21@gmail.com>
2021-03-15 23:45:37 +0000

    Deploying to build from @ polympics/vue-site@9a22f58151bf6fe767b56ef197f9940798305058 🚀

---
+++ precache-manifest.2bb0ea5708464834f5d98efc80e6b4c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.nojekyll"
+  },
+  {
+    "revision": "2d1cd4aa5c6caea9bda45b64477a96b4",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "3459d4e0071ad91ab1b9",
+    "url": "/css/app.3110588b.css"
+  },
+  {
+    "revision": "1ec015a50ef34b48dd6d220ca282c52b",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "6b78a387631e26a3c4ae8730e8bac0a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3459d4e0071ad91ab1b9",
+    "url": "/js/app.150efbb4.js"
+  },
+  {
+    "revision": "4c26d59a9ee1d8a993a0",
+    "url": "/js/chunk-vendors.8b87969f.js"
+  },
+  {
+    "revision": "ea6beefc53f90926c434bc25de12dac4",
+    "url": "/manifest.json"
+  }
+]);
