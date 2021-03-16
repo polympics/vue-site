2c4eba5e32bd22ed762e74d717a935ca958409e7
Artemis21 <artemisdev21@gmail.com>
2021-03-16 00:20:03 +0000

    Consistent indents.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  rules: {
-    test: /\.sass$/,
-    use: [
-      "vue-style-loader",
-      "css-loader",
-      {
-        loader: "sass-loader",
-        options: {
-          sassOptions: {
-            indentedSyntax: true
-          }
-        }
-      }
-    ]
-  }
+    rules: {
+        test: /\.sass$/,
+        use: [
+            "vue-style-loader",
+            "css-loader",
+            {
+                loader: "sass-loader",
+                options: {
+                    sassOptions: {
+                        indentedSyntax: true
+                    }
+                }
+            }
+        ]
+    }
 };
