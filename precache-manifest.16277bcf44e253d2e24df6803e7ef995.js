8462816bbbe95fe349ea61259282a54a6228bc24
Artemis21 <hollyshort21@gmail.com>
2021-03-22 14:54:16 +0000

    Deploying to build from @ polympics/vue-site@b076735a12fce51fd46de1b2c0b601c618d422a1 🚀

--- precache-manifest.16277bcf44e253d2e24df6803e7ef995.js
+++ precache-manifest.16277bcf44e253d2e24df6803e7ef995.js
@@ -4,20 +4,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/common.js"
   },
   {
-    "revision": "07e04a527a8a0e6c65e8",
-    "url": "/css/app.e1dc78c9.css"
+    "revision": "7364e13614ebe6be6a80",
+    "url": "/css/app.a32b5eab.css"
   },
   {
     "revision": "1ec015a50ef34b48dd6d220ca282c52b",
     "url": "/img/logo.png"
   },
   {
-    "revision": "8715e36faaecd1fd1363c32b27d13c9f",
+    "revision": "1c146e54b071404d4737e683db319032",
     "url": "/index.html"
   },
   {
-    "revision": "07e04a527a8a0e6c65e8",
-    "url": "/js/app.ae14aa33.js"
+    "revision": "7364e13614ebe6be6a80",
+    "url": "/js/app.20ebc334.js"
   },
   {
     "revision": "c8e92c7275eea3e215cf",
